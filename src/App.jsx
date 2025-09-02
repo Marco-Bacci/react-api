cd0f3495b531b67df09022ba1c0bfd3b7b2701c5
Marco Bacci <marcobacci94@gmail.com>
2025-09-02 15:32:59 +0200

    feat: card static structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-
 function App() {
-
   return (
-    
-      
-  )
+    <div className="container">
+      <div className="row">
+        <div className="col-12 text-center">
+          <h1>Actors</h1>
+          <p>List of actors fetched from an API</p>
+        </div>
+        <div className="col-4">
+          <div className="box text-center">
+            <h4>nome cognome</h4>
+            <img src="https://cdn.bestmovie.it/wp-content/uploads/2021/06/GettyImages-512916978-scaled.jpg" alt="margot"/>
+            <p><span>anno</span> e <span>nazionalità</span></p>
+            <p>DESCRIZIONE</p>
+            <p>MOVIES</p>
+            <p>OSCAR</p>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
